c9bea64b31dc1336dc62bf33cef367c207a72246
Rustem <scorus0808@gmail.com>
2022-04-14 15:01:29 -0500

    added

--- app/src/test/java/dao/StatusDAOTest.java
+++ app/src/test/java/dao/StatusDAOTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import java.sql.SQLException;
 
 import org.junit.Test;
